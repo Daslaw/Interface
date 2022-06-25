df4d031eb01cb9e1623a29a74496b56b65315897
Dauda Lawal <64631869+Daslaw@users.noreply.github.com>
2022-06-25 22:08:08 +0100

    Interface task 8

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+ 
+public class FirstInterface
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car(0);
+ 
+        int fuel = int.Parse(Console.ReadLine());
+        if(car.Refuel(fuel))
+        {
+            car.Drive();
+        }
+    }
+ 
+    public interface IVehicle
+    {
+        void Drive();
+        bool Refuel(int amount);
+    }
+ 
+    public class Car : IVehicle
+    {
+        public int Fuel { get; set; }
+ 
+        public Car(int fuel)
+        {
+            Fuel = fuel;
+        }
+ 
+        public void Drive()
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine("Driving");
+            }
+            else
+            {
+                Console.WriteLine("Not fuel");
+            }
+        }
+ 
+        public bool Refuel(int amount)
+        {
+            Fuel += amount;
+            return true;
+        }
+    }
+}
